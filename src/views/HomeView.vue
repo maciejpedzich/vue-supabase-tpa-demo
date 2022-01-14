b90a7ab21af92df289ec66bcdab63c1499d8e776
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-14 17:45:26 +0100

    Add dynamic SignIn/Protected link and signOut btn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,28 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+
+import { useAuthStore } from '@/stores/auth';
+import { supabase } from '@/supabase';
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const routerLinkDestination = computed(() =>
+  authStore.isAuthenticated ? 'Protected' : 'SignIn'
+);
+
+const signOut = async () => {
+  await supabase.auth.signOut();
+  authStore.clearUser();
+  router.push('/sign-in');
+};
+</script>
+
 <template>
   <h1>Vue Supabase TPA Demo</h1>
+  <RouterLink :to="{ name: routerLinkDestination }">
+    <p>Go to {{ routerLinkDestination }}</p>
+  </RouterLink>
+  <button v-if="authStore.isAuthenticated" @click="signOut">Sign out</button>
 </template>
