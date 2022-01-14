c968996e593ffc4e80dd24e2f5fea4a974e4da5c
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-14 17:44:21 +0100

    Load auth state on mount and SIGNED_IN event

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+
+import { useAuthStore } from '@/stores/auth';
+import { supabase } from './supabase';
+
+const authStore = useAuthStore();
+
+authStore.loadUser();
+
+supabase.auth.onAuthStateChange(
+  (event) => event === 'SIGNED_IN' && authStore.loadUser()
+);
 </script>
 
 <template>
