0f0713d79e6e2b28229460b92da78e4697b8068b
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-16 22:49:49 +0100

    Only call Supabase's signOut method on signOut

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,25 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
-import { RouterLink, useRouter } from 'vue-router';
+import { RouterLink } from 'vue-router';
 
 import { useAuthStore } from '@/stores/auth';
 import { supabase } from '@/supabase';
 
-const router = useRouter();
 const authStore = useAuthStore();
 
 const routerLinkDestination = computed(() =>
-  authStore.isAuthenticated ? 'Protected' : 'SignIn'
+  authStore.isAuthenticated ? 'Profile' : 'SignIn'
 );
 
 const signOut = async () => {
   await supabase.auth.signOut();
-  authStore.clearUser();
-  router.push('/sign-in');
 };
 </script>
 
 <template>
   <h1>Vue Supabase TPA Demo</h1>
   <RouterLink :to="{ name: routerLinkDestination }">
-    <p>Go to {{ routerLinkDestination }}</p>
+    <p>Go to {{ routerLinkDestination }} page</p>
   </RouterLink>
   <button v-if="authStore.isAuthenticated" @click="signOut">Sign out</button>
 </template>
