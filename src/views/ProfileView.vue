8abf8a6987d28861baa731b685d86ce3b3520d55
Maciej Pędzich <dnalop.me@gmail.com>
2022-01-16 22:38:38 +0100

    Rename Protected view to Profile and redesign it

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router';
+
+import { User } from '@supabase/supabase-js';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+const { user_metadata } = authStore.currentUser as User;
+const { avatar_url, user_name } = user_metadata;
+const avatarAltText = `${user_name}'s avatar`;
+</script>
+
+<template>
+  <h1>{{ user_name }}</h1>
+  <img :src="avatar_url" :alt="avatarAltText" />
+  <RouterLink to="/">
+    <p>Take me back home</p>
+  </RouterLink>
+</template>
--- src/views/ProtectedView.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h1>Protected</h1>
-  <p>If you can see this message, it means you're authenticated</p>
-</template>
